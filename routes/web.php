dd4b0c15cfecd7b17597d46dd6ab3e57ac402cf6
Felix Salcher <dev.felix.salcher@gmail.com>
2024-04-19 18:30:41 +0200

    added home route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
+Route::get('/', HomeController::class)
+    ->name('home');
 
 require __DIR__.'/auth.php';
