42c7f8309dff61d2a12b23caea26d7a7f7328885
Felix Salcher <dev.felix.salcher@gmail.com>
2024-04-22 16:12:05 +0200

    removed filetree and filepreview

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\FileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
@@ -16,14 +15,6 @@ Route::prefix('project')->group(function () {
     Route::post('create', [ProjectController::class, 'store'])
         ->middleware('auth')
         ->name('project.store');
-
-    Route::get('/{project}/dir/{path?}', [FileController::class, 'index'])
-        ->where('path', '.*')
-        ->name('files.index');
-
-    Route::get('/{project}/file/{path?}', [FileController::class, 'show'])
-        ->where('path', '.*')
-        ->name('files.show');
 
     Route::get('/{id}', [ProjectController::class, 'show'])
         ->name('project.show');
