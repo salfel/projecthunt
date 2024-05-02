8003c361a96fc42711cde460e14335cb0b7cdba1
Felix Salcher <dev.felix.salcher@gmail.com>
2024-05-02 10:14:24 +0200

    hide feature dropdown when user is not owner of project

--- resources/js/Pages/Project/Feature/Index.tsx
+++ resources/js/Pages/Project/Feature/Index.tsx
@@ -1,20 +1,21 @@
 import BaseLayout from "@/Layouts/BaseLayout";
 import ProjectLayout from "@/Layouts/ProjectLayout";
 import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
 } from "@/components/ui/accordion";
 import { buttonVariants } from "@/components/ui/button";
 import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuGroup,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
+import { useUser } from "@/lib/hooks";
 import type { Feature as FeatureType, PageProps, Project } from "@/types";
 import { Link } from "@inertiajs/react";
 import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
@@ -22,101 +23,115 @@ import { Pencil, Settings, Trash } from "lucide-react";
 import type { ReactNode } from "react";
 
 interface Props extends PageProps {
-	project: Project;
+    project: Project;
 }
 
 function Features({ project, auth }: Props) {
-	return (
-		<div className="space-y-3">
-			<div className="flex items-center justify-between">
-				<h1>Feature list</h1>
+    return (
+        <div className="space-y-3">
+            <div className="flex items-center justify-between">
+                <h1>Feature list</h1>
 
-				{auth.user?.id === project?.user?.id && (
-					<Link
-						href={route("projects.features.create", [project.id])}
-						className={buttonVariants()}
-					>
-						Create Feature
-					</Link>
-				)}
-			</div>
+                {auth.user?.id === project?.user?.id && (
+                    <Link
+                        href={route("projects.features.create", [project.id])}
+                        className={buttonVariants()}
+                    >
+                        Create Feature
+                    </Link>
+                )}
+            </div>
 
-			<FeatureList features={project.features} />
-		</div>
-	);
+            <FeatureList features={project.features} project={project} />
+        </div>
+    );
 }
 
-function FeatureList({ features }: { features: FeatureType[] }) {
-	return (
-		<Accordion type="multiple" className="space-y-5">
-			{features.map((feature) => (
-				<Feature feature={feature} key={feature.id} />
-			))}
-		</Accordion>
-	);
+function FeatureList({
+    features,
+    project,
+}: { features: FeatureType[]; project: Project }) {
+    const user = useUser();
+    return (
+        <Accordion type="multiple" className="space-y-5">
+            {features.map((feature) => (
+                <Feature
+                    feature={feature}
+                    key={feature.id}
+                    isOwner={user?.id === project.user?.id}
+                />
+            ))}
+        </Accordion>
+    );
 }
 
-function Feature({ feature }: { feature: FeatureType }) {
-	return (
-		<AccordionItem value={feature.name} key={feature.id}>
-			<div className="flex items-center gap-3">
-				<AccordionTrigger className="font-semibold flex-1">
-					<div className="flex-1 text-start">{feature.name}</div>
-				</AccordionTrigger>
+function Feature({
+    feature,
+    isOwner,
+}: { feature: FeatureType; isOwner: boolean }) {
+    return (
+        <AccordionItem value={feature.name} key={feature.id}>
+            <div className="flex items-center gap-3">
+                <AccordionTrigger className="font-semibold flex-1">
+                    <div className="flex-1 text-start">{feature.name}</div>
+                </AccordionTrigger>
+                {isOwner && (
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <button type="button">
+                                <Settings className="size-4" />
+                            </button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end" sideOffset={12}>
+                            <DropdownMenuLabel>Settings</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuGroup>
+                                <DropdownMenuItem
+                                    className="flex items-center gap-3"
+                                    asChild
+                                >
+                                    <Link
+                                        href={route("features.edit", [
+                                            feature.id,
+                                        ])}
+                                    >
+                                        <Pencil className="size-4" />
+                                        <span>Edit</span>
+                                    </Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem
+                                    className="space-x-3 text-red-500 hover:!text-red-600 dark:hover:!text-red-400"
+                                    asChild
+                                >
+                                    <Link
+                                        href={route("features.destroy", [
+                                            feature.id,
+                                        ])}
+                                        method="delete"
+                                        as="button"
+                                        className="w-full"
+                                    >
+                                        <Trash className="size-4" />
+                                        <span>Delete</span>
+                                    </Link>
+                                </DropdownMenuItem>
+                            </DropdownMenuGroup>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                )}
+            </div>
 
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<button type="button">
-							<Settings className="size-4" />
-						</button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end" sideOffset={12}>
-						<DropdownMenuLabel>Settings</DropdownMenuLabel>
-						<DropdownMenuSeparator />
-						<DropdownMenuGroup>
-							<DropdownMenuItem
-								className="flex items-center gap-3"
-								asChild
-							>
-								<Link
-									href={route("features.edit", [feature.id])}
-								>
-									<Pencil className="size-4" />
-									<span>Edit</span>
-								</Link>
-							</DropdownMenuItem>
-							<DropdownMenuItem
-								className="space-x-3 text-red-500 hover:!text-red-600 dark:hover:!text-red-400"
-								asChild
-							>
-								<Link
-									href={route("features.destroy", [
-										feature.id,
-									])}
-									method="delete"
-									as="button"
-									className="w-full"
-								>
-									<Trash className="size-4" />
-									<span>Delete</span>
-								</Link>
-							</DropdownMenuItem>
-						</DropdownMenuGroup>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			</div>
-
-			<AccordionContent>
-				<p>{feature.description}</p>
-			</AccordionContent>
-		</AccordionItem>
-	);
+            <AccordionContent>
+                <p>{feature.description}</p>
+            </AccordionContent>
+        </AccordionItem>
+    );
 }
 
 Features.layout = (page: ReactNode) => (
-	<BaseLayout>
-		<ProjectLayout>{page}</ProjectLayout>
-	</BaseLayout>
+    <BaseLayout>
+        <ProjectLayout>{page}</ProjectLayout>
+    </BaseLayout>
 );
 
 export default Features;
